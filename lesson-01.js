5fc138908c682c5448b2055699feea9eed3e1b75
Luis Santos <luis@luissantos.pt>
2022-01-03 22:25:39 +0100

    Create lesson-01.js

---
+++ lesson-01.js
@@ -0,0 +1,9 @@
+// Lesson 01 variables and assignment
+let name = "John"
+let age = 26
+let hobbies = ["⛷️", "🏄"];
+
+console.log("Personal information")
+console.log("Name: " + name)
+console.log("Age: " + age)
+console.log("Hobbies: " + hobbies)
